5eb350f82fefb4dba83356e72756f9b9e77ed024
KimiPandre <55571522+KimiPandre@users.noreply.github.com>
2019-09-23 14:53:42 +0530

    Update heart_disease.py

--- heart_disease.py
+++ heart_disease.py
@@ -499,6 +499,54 @@ plt.xlabel("False positive rate")
 plt.ylabel("True positive rate")
 plt.show()
 
+#ROC Curve (converting to binary classifier)
+
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.metrics import roc_curve, auc
+
+y = label_binarize(y, classes=[0,1,2])
+n_classes = 3
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+clf = OneVsRestClassifier(LinearSVC(random_state=0))
+y_score = clf.fit(X_train, y_train).decision_function(X_test)
+
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+    
+for i in range(n_classes):
+    plt.figure()
+    plt.plot(fpr[i], tpr[i], label='ROC curve (area = %0.2f)' % roc_auc[i])
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
